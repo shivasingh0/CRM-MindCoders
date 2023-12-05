51cc547130e0969ffbff00965ab4a99677e9bff0
shivasingh0 <shiva2003rajawat@gmail.com>
2023-12-05 18:02:11 +0530

    Working on UI

--- frontend/src/components/NavbarHeader.jsx
+++ frontend/src/components/NavbarHeader.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const NavbarHeader = () => {
-    return (
-        <>
-            <div className="nav-header">
-                <a href="index.html" className="brand-logo">
-                    <img src="images/logo.png" />
-                </a>
-                <div className="nav-control">
-                    <div className="hamburger">
-                        <span className="line" />
-                        <span className="line" />
-                        <span className="line" />
-                    </div>
-                </div>
-            </div>
+  const [isHamburgerActive, setIsHamburgerActive] = useState(false);
 
-        </>
-    )
-}
+  function toggle() {
+    setIsHamburgerActive(!isHamburgerActive);
+    let ele = document.getElementById("main-wrapper");
+    let cls = ele.className;
+    console.log(cls);
+    if (cls === "show") {
+      ele.classList.add("menu-toggle");
+    } else {
+      ele.classList.remove("menu-toggle");
+    }
+  }
 
-export default NavbarHeader
+  return (
+    <>
+      <div className="nav-header">
+        <a href="index.html" className="brand-logo">
+          <img src="images/logo.png" alt="img" />
+        </a>
+        <div className="nav-control">
+          <div
+            className={isHamburgerActive ? "hamburger is-active" : "hamburger"}
+            onClick={() => {
+              toggle();
+            }}
+          >
+            <span className="line" />
+            <span className="line" />
+            <span className="line" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavbarHeader;
